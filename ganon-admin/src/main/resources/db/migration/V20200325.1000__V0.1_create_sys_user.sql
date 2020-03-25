24835175ffe357e049f3cdb83f6f13028379f040
liyustar <liyustar@gmail.com>
2020-03-25 11:30:55 +0800

    feat: integration flyway, h2

---
+++ ganon-admin/src/main/resources/db/migration/V20200325.1000__V0.1_create_sys_user.sql
@@ -0,0 +1,10 @@
+create table sys_user
+(
+  id int not null,
+  name varchar(32) not null,
+  password varchar(32),
+  password_md5 varchar(64),
+  password_sha varchar(64),
+  primary key (id)
+);
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
