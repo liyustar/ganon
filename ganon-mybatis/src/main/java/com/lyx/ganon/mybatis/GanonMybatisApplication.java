5cba51dc4efce315d466073bbad5af98624d3041
liyustar <liyustar@gmail.com>
2020-04-06 14:48:59 +0800

    feat: add module ganon-mybatis

---
+++ ganon-mybatis/src/main/java/com/lyx/ganon/mybatis/GanonMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.lyx.ganon.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanonMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanonMybatisApplication.class, args);
+	}
+
+}
---
+++ ganon-mybatis/src/test/java/com/lyx/ganon/mybatis/GanonMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyx.ganon.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanonMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
