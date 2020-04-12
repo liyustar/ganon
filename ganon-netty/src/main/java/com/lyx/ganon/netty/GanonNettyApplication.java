bda0c8cf5be96bcb2c4d39c9c0eb898e0d1706ab
liyustar <liyustar@gmail.com>
2020-04-12 15:26:05 +0800

    feat: add ganon-netty module

---
+++ ganon-netty/src/main/java/com/lyx/ganon/netty/GanonNettyApplication.java
@@ -0,0 +1,13 @@
+package com.lyx.ganon.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanonNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanonNettyApplication.class, args);
+	}
+
+}
---
+++ ganon-netty/src/test/java/com/lyx/ganon/netty/GanonNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyx.ganon.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GanonNettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
